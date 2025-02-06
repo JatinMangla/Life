f7e59fa642c321b964f76d02ec73b8cc0323c746
Jatin Mangla <jmangla@aapnainfotech.com>
2025-02-07 00:54:19 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -26,14 +26,13 @@ export default defineConfig({
       providerImportSource: '@mdx-js/react',
     }),
     remixCloudflareDevProxy(),
-    // remix({
-    //   routes(defineRoutes) {
-    //     return defineRoutes(route => {
-    //       route('/', 'routes/home/route.js', { index: true });
-    //     });
-    //   },
-    // }),
-    remix(),
+    remix({
+      routes(defineRoutes) {
+        return defineRoutes(route => {
+          route('/', 'routes/home/route.js', { index: true });
+        });
+      },
+    }),
     jsconfigPaths(),
   ],
 });
