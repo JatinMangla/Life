50e719b1e3755d1dba79ecc0253068c04754a373
Jatin Mangla <jmangla@aapnainfotech.com>
2025-02-07 20:56:06 +0530

    update files

--- remix.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-    serverBuildTarget: "vercel",
-    serverModuleFormat: "esm",
-    serverMinify: true,
-    serverDependenciesToBundle: "all",
-  };
-  
