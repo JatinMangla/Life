d545dcc50638bf3c1b1e4ad52bc9f98805e86817
Jatin Mangla <jmangla@aapnainfotech.com>
2025-02-08 03:15:46 +0530

    update conifg

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,8 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-    appDirectory: "app",
-    assetsBuildDirectory: "public/build",
-    serverBuildDirectory: "build",
-    publicPath: "/build/",
-    serverBuildTarget: "vercel", // <-- This tells Remix to build for Vercel
-  };
-  
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  serverBuildDirectory: "build",
+  publicPath: "/build/",
+  serverBuildTarget: "vercel"
+};
